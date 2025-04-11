796770cc5a8199204926adacf57d98db9d6f32a2
skynet <fsrinehart@gmail.com>
2025-04-11 08:58:18 -0400

    Cleaned repo by ignoring large files and MongoDB internals

--- data/import.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "⏳ Importing VIW_FNT_final.csv into MongoDB..."
-
-mongoimport --db myDatabase --collection myCollection 	--type csv --headerline --file /data/VIW_FNT_final.csv 
- 
-
-echo "✅ CSV Import complete!"
